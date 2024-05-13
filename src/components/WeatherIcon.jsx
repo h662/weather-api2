67482f3132dad8d914e7ed35400f08bf404987d1
h662 <h662hong@gmail.com>
2024-05-13 15:30:44 +0900

    latitude, longitude

---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const WeatherIcon = () => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
+  return <div>WeatherIcon</div>;
+};
+
+export default WeatherIcon;
