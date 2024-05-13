43c3abe1a7ca37f6516e9869c9a25fcb77da9543
h662 <h662hong@gmail.com>
2024-05-13 16:35:16 +0900

    Geolocation, Axios

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 
@@ -5,12 +6,32 @@ const WeatherIcon = () => {
   const [latitude, setLatitude] = useState();
   const [longitude, setLongitude] = useState();
 
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.VITE_API_KEY
+        }&units=metric`
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
     });
   }, []);
+
+  useEffect(() => {
+    if (!latitude) return;
+
+    getWeather();
+  }, [latitude]);
 
   return <div>WeatherIcon</div>;
 };
